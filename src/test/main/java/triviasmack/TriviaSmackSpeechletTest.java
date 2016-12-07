7dac25b936825c347d2887ff659db28c41809596
Anna Beddow <anna.beddow@gmail.com>
2016-12-07 15:37:11 +0000

    added test concatenate test

---
+++ src/test/main/java/triviasmack/TriviaSmackSpeechletTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TriviaSmackSpeechletTest {
+
+  @Test
+  public void testConcatenate(){
+    TriviaSmackSpeechlet tss = new TriviaSmackSpeechlet();
+
+    String result = tss.concatenate("one", "two");
+
+    assertEquals("onetwo", result);
+  }
+}
