6514eeb3fefb585ac59615cef3d5680e0970b094
Asif Hafeez <asif.hafeez@hotmail.co.uk>
2016-12-09 15:47:53 +0000

    added tests

--- src/main/java/triviasmack/AnswerHandler.java
+++ src/main/java/triviasmack/AnswerHandler.java
@@ -20,6 +20,11 @@ import com.amazon.speech.ui.SimpleCard;
 
 public class AnswerHandler {
 
+ public String setQuestion(){
+    String question = "<speak>What is the capital of the UK?<break time='50ms'/> 10<break time='50ms'/> 9<break time='50ms'/> 8<break time='50ms'/> 7<break time='50ms'/> 6<break time='50ms'/> 5<break time='50ms'/> 4<break time='50ms'/> 3<break time='50ms'/> 2<break time='50ms'/> 1<break time='50ms'/> TIME'S UP! <break time='50ms'/> The answer is London</speak>";
+    return question;
+  }
+  
   public String checkIfCorrect(final String answer) {
     String correctAnswer = "london";
 
@@ -30,9 +35,6 @@ public class AnswerHandler {
       }
   }
 
-  public String setQuestion(){
-    String question = "<speak>What is the capital of the UK?<break time='50ms'/> 10<break time='50ms'/> 9<break time='50ms'/> 8<break time='50ms'/> 7<break time='50ms'/> 6<break time='50ms'/> 5<break time='50ms'/> 4<break time='50ms'/> 3<break time='50ms'/> 2<break time='50ms'/> 1<break time='50ms'/> TIME'S UP! <break time='50ms'/> The answer is London</speak>";
-    return question;
-  }
+ 
 
 }
